9146ccac894731adadacb219cd437214d22ce0d4
ZMilica <mzivkovic.nis@gmail.com>
2026-01-18 15:07:52 +0100

    Izmenjena migracija za kreiranje tabele users, i dodata migracija za kreiranje tabele zaposleni

--- PROJEKAT/back/database/migrations/0001_01_01_000000_create_users_table.php
+++ PROJEKAT/back/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('ime');
+            $table->string('prezime');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('type', ['vlasnica', 'klijent', 'sminkerka','manikirka']); 
             $table->rememberToken();
             $table->timestamps();
         });
